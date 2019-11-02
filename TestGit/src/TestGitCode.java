fd7ef809ba8794230565b004fe8300f38782b937
Srivastava, Gaurav(gs9442) <gs9442@att.com>
2019-11-02 13:13:01 +0530

    first version of git code commit

---
+++ TestGit/src/TestGitCode.java
@@ -0,0 +1,8 @@
+
+public class TestGitCode {
+
+	public static void main(String[] args) {
+		System.out.println("Committed the code successfully !");
+	}
+	
+}
---
+++ TestGit/src/TestGitData.java
@@ -0,0 +1,8 @@
+
+public class TestGitData {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GIT !");
+	}
+	
+}
