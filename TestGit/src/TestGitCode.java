b2855ded3f0d90f425d5ae6fa7593944f51e97e8
Srivastava, Gaurav(gs9442) <gs9442@att.com>
2019-11-03 12:13:14 +0530

    Revert "third version of code commit to undo the committed code changes"
    This reverts commit 5aa0efc52f12350ffdb7001417e7076bb50d0cac.

--- TestGit/src/TestGitCode.java
+++ TestGit/src/TestGitCode.java
@@ -4,7 +4,6 @@ public class TestGitCode {
 	public static void main(String[] args) {
 		System.out.println("Committed the code successfully !");
 		System.out.println("Committed the code successfully second time !");
-		System.out.println("Code added to test changes of 2 files !");
 	}
 	
 }
