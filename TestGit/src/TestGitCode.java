baa77b18b332ecac893493a3d5c9df6fc35cb390
Srivastava, Gaurav(gs9442) <gs9442@att.com>
2019-11-03 12:28:34 +0530

    second version of git commit for revert command

--- TestGit/src/TestGitCode.java
+++ TestGit/src/TestGitCode.java
@@ -3,7 +3,6 @@ public class TestGitCode {
 
 	public static void main(String[] args) {
 		System.out.println("Committed the code successfully !");
-		System.out.println("Committed the code successfully second time !");
 	}
 	
 }
