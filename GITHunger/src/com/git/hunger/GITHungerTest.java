e6453f167f9dd503378a4c1c605c10c5c1184456
Srivastava, Gaurav(gs9442) <gs9442@att.com>
2019-11-03 12:57:57 +0530

    "hungery guy for learning"

--- GITHunger/src/com/git/hunger/GITHungerTest.java
+++ GITHunger/src/com/git/hunger/GITHungerTest.java
@@ -3,7 +3,7 @@ package com.git.hunger;
 public class GITHungerTest {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello Hungry Guy !");
+		System.out.println("Hello Hungry Guy for learning !");
 	}
 
 }
